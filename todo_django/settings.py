7647246dc76096a6f068f82e39fe57eda171e6c1
Luiz Filipi <luizfilipi3246@gmail.com>
2024-09-19 17:43:45 -0300

    atualizacao

--- todo_django/settings.py
+++ todo_django/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+]
+INSTALLED_APPS += [
     'tasks'
 ]
 
@@ -55,7 +58,7 @@ ROOT_URLCONF = 'todo_django.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
